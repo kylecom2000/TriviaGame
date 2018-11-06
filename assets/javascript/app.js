5ef426f967a74daa3a470b93b15e0bdc9730c6c1
FightingSprrow <kbauertx@gmail.com>
2018-11-05 20:13:46 -0600

    Finished game

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,56 +6,56 @@ var questionChoicesImageAnswer = [
     image:  ["assets/images/01banana.gif"],
     answer: 1
     }, 
-    // {
-    // // question 01
-    // question:"In what 1985 film did Val Kilmer turn a professor’s house into a giant popcorn maker?",
-    // choices: ["Real Genius", "The Matrix", "WarGames", "Top Gun"],
-    // image: ["assets/images/02realGenius.gif"],
-    // answer: 0
-    // }, {
-    // // question 02
-    // question: "He did Danger Zone in 86 and Footloose in 84, what song did Kenny Loggins create for a 1980 film?",
-    // choices: ["Love Will Follow", "What a Fool Believes", "If You Believe", "I'm All Right" ],
-    // image:  ["assets/images/03caddyshack.gif"],
-    // answer: 3
-    // }, {
-    // // question 03
-    // question: "What popular 80s group got their name from the 1960s Jane Fonda film Barbarella?",
-    // choices: ["The Cure", "Duran Duran", "Depeche Mode", "Bon Jovi" ],
-    // image:  ["assets/images/04barbarella.gif"],
-    // answer: 1
-    // }, {
-    // // question 04
-    // question: "What father/daughter pair released a single 'Valley Girl' which was intended to mock the valley girl image?",
-    // choices: ["will and Willow Smith", "Miley and Billy Ray Cyrus", "Frank and Moon Unit Zappa", "Johnny and Rosanne Cash" ],
-    // //zappa on the shitter.
-    // image:  ["assets/images/05zappa.jpg"],
-    // answer: 2
-    // }, {
-    // // question 05
-    // question: "His first ever role was on an episode of Clarissa Explains It All in 1993, but what actor broke into the film world by not wanting his father’s life in 1999?",
-    // choices: ["Justin Timberlake", "James Van Der Beek", "Tom Cruise", "Macaulay Culkin" ],
-    // image:  ["assets/images/06vanderbeek.gif"],
-    // answer: 1
-    // }, {
-    // // question 06
-    // question: "What did Bill Murray say he would’ve named Owen Wilson in The Life Aquatic with Steve Zissou?",
-    // choices: ["Kingsley", "Bradon", "Chad", "Harlowe" ],
-    // image:  ["assets/images/07murray.gif"],
-    // answer: 0
-    // }, {
-    // // question 07
-    // question: "Born in 1920, what famous American author is best known for his 1953 novel Fahrenheit 451?",
-    // choices: ["Robert A. Heinlein", "Orson Welles", "Ray Bradbury", "L. Ron Hubbord" ],
-    // image:  ["assets/images/08books.gif"],
-    // answer: 2
-    // }, {
-    // // question 08
-    // question: "What was the first and last name of Ray Liotta’s character in Martin Scorsese’s 1990 gangster flick Goodfellas?",
-    // choices: ["Henry Hill", "Paul Cicero", "Fat Andy", "Mikey Franzese" ],
-    // image:  ["assets/images/09liotta.gif"],
-    // answer: 0
-    // }, 
+    {
+    // question 01
+    question:"In what 1985 film did Val Kilmer turn a professor’s house into a giant popcorn maker?",
+    choices: ["Real Genius", "The Matrix", "WarGames", "Top Gun"],
+    image: ["assets/images/02realGenius.gif"],
+    answer: 0
+    }, {
+    // question 02
+    question: "He did Danger Zone in 86 and Footloose in 84, what song did Kenny Loggins create for a 1980 film?",
+    choices: ["Love Will Follow", "What a Fool Believes", "If You Believe", "I'm All Right" ],
+    image:  ["assets/images/03caddyshack.gif"],
+    answer: 3
+    }, {
+    // question 03
+    question: "What popular 80s group got their name from the 1960s Jane Fonda film Barbarella?",
+    choices: ["The Cure", "Duran Duran", "Depeche Mode", "Bon Jovi" ],
+    image:  ["assets/images/04barbarella.gif"],
+    answer: 1
+    }, {
+    // question 04
+    question: "What father/daughter pair released a single 'Valley Girl' which was intended to mock the valley girl image?",
+    choices: ["will and Willow Smith", "Miley and Billy Ray Cyrus", "Frank and Moon Unit Zappa", "Johnny and Rosanne Cash" ],
+    //zappa on the shitter.
+    image:  ["assets/images/05zappa.jpg"],
+    answer: 2
+    }, {
+    // question 05
+    question: "His first ever role was on an episode of Clarissa Explains It All in 1993, but what actor broke into the film world by not wanting his father’s life in 1999?",
+    choices: ["Justin Timberlake", "James Van Der Beek", "Tom Cruise", "Macaulay Culkin" ],
+    image:  ["assets/images/06vanderbeek.gif"],
+    answer: 1
+    }, {
+    // question 06
+    question: "What did Bill Murray say he would’ve named Owen Wilson in The Life Aquatic with Steve Zissou?",
+    choices: ["Kingsley", "Bradon", "Chad", "Harlowe" ],
+    image:  ["assets/images/07murray.gif"],
+    answer: 0
+    }, {
+    // question 07
+    question: "Born in 1920, what famous American author is best known for his 1953 novel Fahrenheit 451?",
+    choices: ["Robert A. Heinlein", "Orson Welles", "Ray Bradbury", "L. Ron Hubbord" ],
+    image:  ["assets/images/08books.gif"],
+    answer: 2
+    }, {
+    // question 08
+    question: "What was the first and last name of Ray Liotta’s character in Martin Scorsese’s 1990 gangster flick Goodfellas?",
+    choices: ["Henry Hill", "Paul Cicero", "Fat Andy", "Mikey Franzese" ],
+    image:  ["assets/images/09liotta.gif"],
+    answer: 0
+    }, 
     {
     // question 09
     question: "What now retired quarterback was fined $50,000 for allegedly sexting a reporter in 2008 and was temporarily banned by the NFL from drinking alcohol after admitting he was addicted to Vicodin?",
@@ -75,6 +75,32 @@ $(document).ready(function() {
     var currentQuestion = 0;
     var correct = 0;
     var notCorrect = 0;
+    var countdown;
+    var countdownNumber;
+    
+    function countdown_init() {
+        countdownNumber = 30;
+        countdownTrigger();
+    }
+    
+    function countdownTrigger() {
+        // console.log("countdownTrigger function called");
+        if(countdownNumber > 0) {
+            countdownNumber--;
+            if (countdownNumber === 0){
+                timesUp();
+            }
+            $("#timerHTML").text(countdownNumber);
+            if(countdownNumber > 0) {
+                countdown = setTimeout(countdownTrigger , 1000);
+            }
+        }
+    }
+    
+    function countdownClear() {
+        clearTimeout(countdown);
+    }
+
 
     // ___________________________________________________________
     // On startup of html document, run this function....
@@ -108,22 +134,16 @@ $(document).ready(function() {
         // display scared wizard of oz gif for 4 seconds
         $("#triviaHTML").append("<img src='assets/images/scared.gif'>");
         // $("#triviaHTML").append("<br>")
-        setTimeout(game, 3000);
+        setTimeout(game, 5000);
     }
 
     function clearHTML() {
         console.log("clearHTML function called");
         // clear html for next function
+        $("#timerHTML").empty();
         $("#triviaHTML").empty();
     }
 
-    function timerStart() {
-        // set timer 30 seconds,
-        setTimeout(timesUp, 30000);
-        
-        // display timer to HTML
-        // $("#triviaHTML").append
-    }
 
     function addQuestion() {
         console.log("addQuestion function called");
@@ -183,7 +203,7 @@ $(document).ready(function() {
         notCorrect++;
         clearHTML();
         // TIMER STOP
-
+        countdownClear();
         // DISPLAY TIMER
 
         // display score
@@ -197,7 +217,7 @@ $(document).ready(function() {
         // add one to current question counter.
        
         currentQuestion++;
-        setTimeout(game, 4000);
+        setTimeout(game, 5000);
     }
 
     function rightAnswer() {
@@ -205,7 +225,7 @@ $(document).ready(function() {
         correct++;
         clearHTML();
         // Timer stop
-
+        countdownClear();
         // timer display
 
         // display score
@@ -218,15 +238,18 @@ $(document).ready(function() {
         
         currentQuestion++;
 
-        setTimeout(game, 4000);
+        setTimeout(game, 5000);
     }
 
     function timesUp() {
         console.log("timesUp finction called");
         //display stopped time
-
+        clearHTML();
+        countdownClear();
         // say TIMES UP, 
         displayScore();
+        // message
+        $("#triviaHTML").append("<h3>You ran out of time!</h3><br>");
         // and the correct answer
         displayAnswerText();
         // call current question gif.
@@ -234,17 +257,7 @@ $(document).ready(function() {
         // add one to current question counter.
         currentQuestion++;
         // keep screen up for 4? seconds.
-        setTimeout(game, 4000);
-    }
-
-    function checkEndGame() {
-        console.log("checkEndGame function called");
-        // if (currentQuestion >= questionChoicesImageAnswer.length) {
-        //     endGame();
-        // } 
-        // else {
-        //     game();
-        // }
+        setTimeout(game, 5000);
     }
 
     function game(){
@@ -254,6 +267,7 @@ $(document).ready(function() {
             clearHTML();
             addQuestion();
             choiceButtons();
+            countdown_init();
             $(".choice").click(choicesClicked);
         } 
         else {
